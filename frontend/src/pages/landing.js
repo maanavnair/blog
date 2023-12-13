391bdc65b47611df7249614a0b66836c46ef32ae
Maanav Nair <maanavnair20042@gmail.com>
2023-12-13 18:20:21 +0530

    made landing page

--- frontend/src/pages/landing.js
+++ frontend/src/pages/landing.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import Navbar from '../components/navbar'
+import heroImage from '../images/heroImage.png'
 
 const Landing = () => {
   return (
-    <div>
-        <h1>Landing Page</h1>
+    <div className='hero-section'>
+        <div className='hero-content'>
+          <h1>Discover Perspectives: Insights, Stories, and Ideas</h1>
+          <p className='hero-para'>Embark on a Journey Through Captivating Narratives, Thought-Provoking Insights, and Inspiring Ideas. 
+            Unleash the Power of Words to Explore, Learn, and Connect – Where Every Blog is a Portal to a World 
+            of Endless Possibilities</p>
+          <p className='hero-ending'>Sign Up Now!</p>
+        </div>
+        <div className='hero-image'>
+          <img src={heroImage} alt='hero-image' />
+        </div>
     </div>
   )
 }
